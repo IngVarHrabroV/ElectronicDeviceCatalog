6caffb637052de6a2e8797b6a37b178ee25b1158
IngVar HrabroV <ingvarhrabrov@gmail.com>
2021-11-23 20:49:50 +0300

    add start config for app, add GreetingController and greeting page

---
+++ src/main/java/com/hrabrov/electronic_device_catalog/Application.java
@@ -0,0 +1,13 @@
+package com.hrabrov.electronic_device_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
