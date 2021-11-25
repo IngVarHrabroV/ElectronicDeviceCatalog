58bc00e944964770d3121e0d0774857f80208395
IngVar HrabroV <ingvarhrabrov@gmail.com>
2021-11-26 02:31:36 +0300

    add authorization

---
+++ src/main/java/com/hrabrov/electronic_device_catalog/config/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.hrabrov.electronic_device_catalog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
