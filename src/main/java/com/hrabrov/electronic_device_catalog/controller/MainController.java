1270b23ff04a4523c8e83154d78427856d9ddd45
IngVar HrabroV <ingvarhrabrov@gmail.com>
2021-11-28 13:31:13 +0300

    add ProductController and split method about product from MainController to ProductController

--- src/main/java/com/hrabrov/electronic_device_catalog/controller/MainController.java
+++ src/main/java/com/hrabrov/electronic_device_catalog/controller/MainController.java
@@ -1,12 +1,9 @@
 package com.hrabrov.electronic_device_catalog.controller;
 
 import com.hrabrov.electronic_device_catalog.domain.Product;
-import com.hrabrov.electronic_device_catalog.domain.User;
 import com.hrabrov.electronic_device_catalog.repositories.ProductsRepository;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -21,7 +18,7 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting() {
         return "greeting";
     }
 
@@ -42,65 +39,6 @@ public class MainController {
 
             model.put("products", findProducts);
         }
-
-        return "main";
-    }
-
-    @PostMapping("/main")
-    public String addProducts(
-            @RequestParam String productName,
-            @RequestParam String category,
-            Map<String, Object> model
-    ) {
-        Product newProduct = new Product(productName, category);
-        productsRepository.save(newProduct);
-
-        Iterable<Product> allProducts = productsRepository.findAll();
-        model.put("products", allProducts);
-
-        return "main";
-    }
-
-    @PostMapping("/delete")
-    public String addProducts(@RequestParam Integer productID, Map<String, Object> model) {
-        if (!productsRepository.existsById(productID)) {
-            model.put("idForDeleteDidntFound", true);
-            model.put("deletingId", productID);
-            return "main";
-        }
-
-        productsRepository.delete(productsRepository.findById(productID));
-
-        Iterable<Product> allProducts = productsRepository.findAll();
-        model.put("products", allProducts);
-
-        return "main";
-    }
-
-    @PostMapping("/edit")
-    public String editProduct(@RequestParam Integer productID,
-                              @RequestParam String kindOfEditInformation,
-                              @RequestParam String editInformation,
-                              Map<String, Object> model) {
-
-        if (!productsRepository.existsById(productID)) {
-            model.put("idForEditDidntFound", true);
-            model.put("editingId", productID);
-            return "main";
-        }
-
-        Product productForEdit = productsRepository.findById(productID);
-
-        if (kindOfEditInformation.equals("editProductName")) {
-            productForEdit.setProductName(editInformation);
-        } else {
-            productForEdit.setCategory(editInformation);
-        }
-
-        productsRepository.save(productForEdit);
-
-        Iterable<Product> allProducts = productsRepository.findAll();
-        model.put("products", allProducts);
 
         return "main";
     }
---
+++ src/main/java/com/hrabrov/electronic_device_catalog/controller/ProductController.java
@@ -0,0 +1,77 @@
+package com.hrabrov.electronic_device_catalog.controller;
+
+import com.hrabrov.electronic_device_catalog.domain.Product;
+import com.hrabrov.electronic_device_catalog.repositories.ProductsRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class ProductController {
+    private final ProductsRepository productsRepository;
+
+    public ProductController(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    @PostMapping("/addProduct")
+    public String addProducts(
+            @RequestParam String productName,
+            @RequestParam String category,
+            Map<String, Object> model
+    ) {
+        Product newProduct = new Product(productName, category);
+        productsRepository.save(newProduct);
+
+        Iterable<Product> allProducts = productsRepository.findAll();
+        model.put("products", allProducts);
+
+        return "main";
+    }
+
+    @PostMapping("/deleteProduct")
+    public String addProducts(@RequestParam Integer productID, Map<String, Object> model) {
+        if (!productsRepository.existsById(productID)) {
+            model.put("idForDeleteDidntFound", true);
+            model.put("deletingId", productID);
+            return "main";
+        }
+
+        productsRepository.delete(productsRepository.findById(productID));
+
+        Iterable<Product> allProducts = productsRepository.findAll();
+        model.put("products", allProducts);
+
+        return "main";
+    }
+
+    @PostMapping("/editProduct")
+    public String editProduct(@RequestParam Integer productID,
+                              @RequestParam String kindOfEditInformation,
+                              @RequestParam String editInformation,
+                              Map<String, Object> model) {
+
+        if (!productsRepository.existsById(productID)) {
+            model.put("idForEditDidntFound", true);
+            model.put("editingId", productID);
+            return "main";
+        }
+
+        Product productForEdit = productsRepository.findById(productID);
+
+        if (kindOfEditInformation.equals("editProductName")) {
+            productForEdit.setProductName(editInformation);
+        } else {
+            productForEdit.setCategory(editInformation);
+        }
+
+        productsRepository.save(productForEdit);
+
+        Iterable<Product> allProducts = productsRepository.findAll();
+        model.put("products", allProducts);
+
+        return "main";
+    }
+}
